46c64bcfdd83c2379d8c0f868ced2b033e3df8c4
yoshimo <109893020+yoshi-mo@users.noreply.github.com>
2022-12-29 14:05:05 +0900

    Update settings.py
    Debug=False

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9ot-5!pmqj%fj0gz&5va#!ir@$x1bclwn0wg_rv&qf!!p$%#*m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
